b94e8f2838b0d7dcba004503d4e5d3aa8c5f8f75
Shirish Kadam <shirishkadam35@yahoo.com>
2018-03-31 09:42:49 +0530

    Extracts tweets meta data with time

--- tweetscap/user_tweets.py
+++ tweetscap/user_tweets.py
@@ -53,8 +53,8 @@ def get_tweet(save_json=False):
 
         tweets_pattern = '''/html/body/li[contains(@class,"stream-item")]'''        
 
-        tweet_content_pattern = '''/div/div[@class="content"]'''
-        tweet_time_ms_pattern = '''/div[@class="stream-item-header"]/small[@class="time"]/a[contains(@class,"tweet-timestamp")]/span/@data-time-ms'''
+        tweet_content_pattern = '''./div[@class="content"]'''
+        tweet_time_ms_pattern = '''./div[@class="stream-item-header"]/small[@class="time"]/a[contains(@class,"tweet-timestamp")]'''
         tweet_text_pattern = '''/div[@class="js-tweet-text-container"]//text()'''
         tweet_links_list_pattern = '''/div[@class="js-tweet-text-container"]//a'''
         
@@ -65,11 +65,16 @@ def get_tweet(save_json=False):
         for tweet in tweet_list:
             item_id = tweet.attrib['data-item-id']
             item_type = tweet.attrib['data-item-type']
-            tweet_id = tweet.getchildren()[0].attrib['data-tweet-id']
-            tweet_author = tweet.getchildren()[0].attrib['data-screen-name']
-            tweet_author_id = tweet.getchildren()[0].attrib['data-user-id']            
+            if len(tweet.getchildren()) > 0:
+                tweet_data = tweet.getchildren()[0]
+                tweet_id = tweet_data.attrib['data-tweet-id']
+                tweet_author = tweet_data.attrib['data-screen-name']
+                tweet_author_id = tweet_data.attrib['data-user-id']
+            
+                tweet_content = tweet_data.xpath(tweet_content_pattern)
+                tweet_time_span = tweet_content[0].xpath(tweet_time_ms_pattern)[0].attrib['title']       # @data-time-ms     
 
-            logger.debug("{0}:{1}:{2}-{3}:{4}".format(item_id, item_type, tweet_id, tweet_author, tweet_author_id))
+                logger.debug("{0}:{1}:{2}-{3}:{4}-{5}".format(item_id, item_type, tweet_id, tweet_author, tweet_author_id, tweet_time_span))
 
 
     except KeyError:
